22917daae8d819f776f953a5836982174afd8891
Saul-Chavez15 <saul.maccleshdelpino@iest.edu.mx>
2026-03-27 23:49:51 -0600

    Add files via upload

--- Cronometro/Cronometro/Form1.Designer.vb
+++ Cronometro/Cronometro/Form1.Designer.vb
@@ -23,127 +23,179 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.Label1 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Button2 = New System.Windows.Forms.Button()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
         Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(274, 72)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(57, 63)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "0"
-        '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(202, 176)
+        Me.Button1.Location = New System.Drawing.Point(269, 217)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.Size = New System.Drawing.Size(100, 28)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Start"
         Me.Button1.UseVisualStyleBackColor = True
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(443, 176)
+        Me.Button2.Location = New System.Drawing.Point(591, 217)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.Size = New System.Drawing.Size(100, 28)
         Me.Button2.TabIndex = 2
         Me.Button2.Text = "Reset"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'Timer1
         '
+        Me.Timer1.Interval = 1000
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(215, 9)
+        Me.Label2.Location = New System.Drawing.Point(287, 11)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(312, 63)
+        Me.Label2.Size = New System.Drawing.Size(384, 76)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "Cronometro"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(341, 118)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(292, 76)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "00:00:00"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(398, 72)
+        Me.Label3.Location = New System.Drawing.Point(28, 33)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(57, 63)
-        Me.Label3.TabIndex = 4
-        Me.Label3.Text = "0"
+        Me.Label3.Size = New System.Drawing.Size(111, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Mira a la derecha"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(337, 72)
+        Me.Label4.Location = New System.Drawing.Point(836, 33)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(42, 63)
-        Me.Label4.TabIndex = 5
-        Me.Label4.Text = ":"
+        Me.Label4.Size = New System.Drawing.Size(107, 16)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Mira hacia abajo"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(493, 72)
+        Me.Label5.Location = New System.Drawing.Point(743, 373)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(57, 63)
-        Me.Label5.TabIndex = 6
-        Me.Label5.Text = "0"
+        Me.Label5.Size = New System.Drawing.Size(112, 16)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "Alarga la ventana"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 40.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(445, 72)
+        Me.Label6.Location = New System.Drawing.Point(444, 427)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(42, 63)
-        Me.Label6.TabIndex = 7
-        Me.Label6.Text = ":"
+        Me.Label6.Size = New System.Drawing.Size(38, 16)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "00:00"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(183, 526)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "1 Minuto"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(505, 538)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "5 Minutos"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(359, 542)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 11
+        Me.Button5.Text = "Inico"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(663, 546)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 12
+        Me.Button6.Text = "Reinicio"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 1000
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(1067, 407)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents Label1 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents Button2 As Button
     Friend WithEvents Timer1 As Timer
     Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
     Friend WithEvents Timer2 As Timer
 End Class
--- Cronometro/Cronometro/Form1.vb
+++ Cronometro/Cronometro/Form1.vb
@@ -1,19 +1,103 @@
 ﻿Public Class Form1
 
-    Dim boton_Start As Boolean = True
-    Dim tiempo_crono As Integer = 0
+    Dim horas As Integer = 0
+    Dim minutos As Integer = 0
+    Dim segundos As Integer = 0
+    Dim Inicio As Boolean = False
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim numero1 As Integer = 1000
-        Dim resultado As Integer = numero1 / 1000
-        Dim residuo As Integer = numero1 Mod 60
-        Label1.Text = resultado.ToString()
-        Label3.Text = residuo.ToString()
 
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Inicio = False Then
+            Timer1.Start()
+            Button1.Text = "Pausar"
+            Inicio = True
+        Else
+            Timer1.Stop()
+            Button1.Text = "Iniciar"
+            Inicio = False
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+        horas = 0
+        minutos = 0
+        segundos = 0
+        Inicio = False
+        Label1.Text = "00:00:00"
+        Button1.Text = "Iniciar"
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        tiempo_crono = tiempo_crono + 1
-        Label1.Text = tiempo_crono.ToString()
+        segundos += 1
+        If segundos = 60 Then
+            segundos = 0
+            minutos += 1
+
+        End If
+        If minutos = 60 Then
+            minutos = 0
+            horas += 1
+        End If
+        Label1.Text =
+        horas.ToString("00") & ":" &
+        minutos.ToString("00") & ":" &
+            segundos.ToString("00")
     End Sub
+
+    Dim tiempoTotal As Integer = 0
+    Dim timerActivo As Boolean = False
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        tiempoTotal = 60
+        ActualizarTimer()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        tiempoTotal = 300
+        ActualizarTimer()
+
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If timerActivo = False And tiempoTotal > 0 Then
+            Timer2.Start()
+            Button5.Text = "Pausar"
+            timerActivo = True
+        Else
+            Timer2.Stop()
+            Button5.Text = "Iniciar"
+            timerActivo = False
+
+
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Timer2.Stop()
+        tiempoTotal = 0
+        Label6.Text = "00:00"
+        Button5.Text = "Iniciar"
+        timerActivo = False
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If tiempoTotal > 0 Then
+            tiempoTotal -= 1
+            ActualizarTimer()
+        Else
+            Timer2.Stop()
+            timerActivo = False
+            Button5.Text = "Iniciar"
+            MessageBox.Show("Tiempo termiando!")
+        End If
+    End Sub
+    Private Sub ActualizarTimer()
+        Dim min As Integer = tiempoTotal / 60
+        Dim seg As Integer = tiempoTotal Mod 60
+        Label6.Text = min.ToString("00") & ":" & seg.ToString("00")
+    End Sub
+
+
+
 End Class
